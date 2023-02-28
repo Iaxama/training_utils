963cb2020f9381ef2b575f695e4015e47c385227
Dario Di Domenico <dario.didomenico@iit.it>
2023-02-28 18:41:04 +0100

    Added methods to filter EMGs

--- training_utils/features_computation.py
+++ training_utils/features_computation.py
@@ -1,8 +1,22 @@
 import numpy as np
+from scipy.signal import butter, filtfilt
+
+def butter_bandpass(lowcut, highcut, fs, order=4):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=4):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    filtered_data = np.zeros_like(data)
+    for i in range(data.shape[0]):
+        filtered_data[i, :] = filtfilt(b, a, data[i, :])
+    return filtered_data
 
 def applyRMS(data):
     return np.sqrt(np.mean(np.square(data), axis=0))
-
 
 def compute_features(data, window, increment, features_list):
     features = []
