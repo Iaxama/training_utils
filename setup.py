ae9e7ae44507c9e6ca12573b39e2a1e4d1bb4369
Massimiliano Iacono <massimiliano.iacono@iit.it>
2024-05-02 16:04:01 +0200

    Updated scipy version in setup

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from setuptools import setup, find_packages, Require
 setup(name='emg-training-utils', 
       version=1.0,
       install_requires=['numpy>=1.19.1,<=1.26.4',
-                        'scipy==1.12.0'],
+                        'scipy>=1.10.0'],
       packages=find_packages())
