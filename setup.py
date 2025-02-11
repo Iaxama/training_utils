e7aa0b91402252ef3ac123a75505c8265af6ffbf
Massimiliano Iacono <massimiliano.iacono@iit.it>
2025-02-11 11:27:30 +0100

    Version 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages, Require
 
 setup(name='emg-training-utils', 
-      version=1.0,
+      version=1.1,
       install_requires=['numpy>=1.19.1,<=1.26.4',
                         'scipy>=1.10.0'],
       packages=find_packages())
